1d5b3dee894bcdd9d5b8f4a8b227dcaa7ce6d1f9
Koding <j.e.bates@outlook.com>
2015-11-18 22:40:41 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FBLA::Application.config.secret_token = '3b568332cc74eaa033a7aab046379fc764e445ca7081cc66d1fb63318d0da00a1f6528f9e3fa5b6514169bcb0de397ebf38737373ea3808bfbf20ce2164746c9'
